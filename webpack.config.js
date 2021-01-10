45121de4472e9324b0c400aaceafc09205d405a5
Erick Peniche <erick@myrodi.com>
2021-01-10 16:46:32 -0600

    Update webpack config with eslint plugin and babel rules for transpiling.Added eslint config file. Added cross-env and nodemon webpack plugin. Updated package.json start script

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const ESLintPlugin = require('eslint-webpack-plugin');
+const NodemonPlugin = require('nodemon-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,5 +8,28 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js', 'json'],
+      fix: true,
+    }),
+    new NodemonPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
   },
 };
